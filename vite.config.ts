5537d6352db7ed9787609a77cf03c09d01d1ba9a
AdityaRepos <adityarajv252@gmail.com>
2026-01-04 18:06:06 +0530

    deployment to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/iitk-placement-2026/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
